813da5e0410883d3bcc0340b4bde4d7bf2117675
Barbara <5elementszone@gmail.com>
2025-03-10 19:19:22 +0100

    finished ex2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,44 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade procentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        string letter;
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats!");
+
+            if (grade >= 90)
+            {
+                letter = "A";
+            }
+            else if (grade >= 80)
+            {
+                letter = "B";
+            }
+            else
+            {
+                letter = "C";
+            }
+        }
+        else
+        {
+            Console.WriteLine("Sorry, good luck next time.");
+
+            if (grade >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}.");
     }
 }
