849cd22ba89c5315b95665d96ffb67c66fe19826
Barbara <5elementszone@gmail.com>
2025-03-14 18:52:00 +0100

    finished journal

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,9 +1,27 @@
 public class PromptGenerator
 {
-    public List<string> _prompts;
+    public List<string> _prompts = new List<string>
+    {
+        "What was the best thing that happened to me today?",
+        "What is one thing I learned today?",
+        "How did I make progress toward my goals today?",
+        "What am I grateful for today?"
+    };
 
     public string GetRandomPrompt()
     {
-        return "";
+        if (_prompts.Count == 0)
+        {
+            return "No questions available.";
+        }
+        else
+        {
+            Random rand = new Random();
+            int index = rand.Next(_prompts.Count);
+            string selectedPrompt = _prompts[index];
+
+            _prompts.RemoveAt(index);
+            return selectedPrompt;
+        }
     }
 }
