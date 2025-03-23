416c498a09915e94e831973e312c04a47c4e70ca
Barbara <5elementszone@gmail.com>
2025-03-23 13:48:30 +0100

    added fraction class

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,48 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int value)
+    {
+        _top = value;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int value)
+    {
+        _bottom = value;
+    }
+    public override string ToString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6,7);
+
+        Console.WriteLine($"{f1} {f2} {f3}");
+
+        Fraction f = new Fraction();
+
+        Console.WriteLine($"Original fraction: {f}");
+
+        f.SetTop(5);
+        f.SetBottom(8);
+
+        Console.WriteLine("\nUpdated Values:");
+        Console.WriteLine($"Top: {f.GetTop()}");
+        Console.WriteLine($"Bottom: {f.GetBottom()}");
+
+        
     }
 }
