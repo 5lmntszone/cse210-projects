dc35a27ecf9a36dca21dc153d22f39b9836a82bd
Barbara <5elementszone@gmail.com>
2025-04-05 15:59:00 +0200

    added classes

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address addr1 = new Address("123 Apple St", "New York", "NY", "USA");
+        Customer cust1 = new Customer("Alice Johnson", addr1);
+        Order order1 = new Order(cust1);
+        order1.AddProduct(new Product("Notebook", "A101", 3.50, 4));
+        
     }
 }
