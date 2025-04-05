2c125422a47b57673c0423fd200311f94b1fb9bf
Barbara <5elementszone@gmail.com>
2025-04-05 21:33:17 +0200

    finished OnlineOrdering

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -8,6 +8,23 @@ class Program
         Customer cust1 = new Customer("Alice Johnson", addr1);
         Order order1 = new Order(cust1);
         order1.AddProduct(new Product("Notebook", "A101", 3.50, 4));
-        
+        order1.AddProduct(new Product("Pen", "B202", 1.25, 10));
+        order1.AddProduct(new Product("Pillow", "A576", 30.00, 2));
+
+        Address addr2 = new Address("456 Maple Ave", "Toronto", "ON", "Canada");
+        Customer cust2 = new Customer("Carlos Ruiz", addr2);
+        Order order2 = new Order(cust2);
+        order2.AddProduct(new Product("Backpack", "C303", 25.00, 1));
+        order2.AddProduct(new Product("Binder", "D404", 4.75, 2));
+
+        List<Order> orders = new List<Order> {order1, order2};
+
+        foreach (Order order in orders)
+        {
+            Console.WriteLine(order.GetPackingLabel());
+            Console.WriteLine(order.GetShippingLabel());
+            Console.WriteLine($"Total Cost: ${order.GetTotalCost():F2}\n");
+        }
+
     }
 }
