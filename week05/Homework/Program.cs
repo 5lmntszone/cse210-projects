aa42178918cb909d9402e74ce7e28985f069f6a2
Barbara <5elementszone@gmail.com>
2025-04-09 22:46:18 +0200

    finished homework ex

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assgn1 = new Assignment("Kasia", "Cats");
+        Console.WriteLine(assgn1.GetSummary());
+
+        MathAssignment assgn2 = new MathAssignment("Jakub", "Cars", "7.3", "8-19");
+        Console.WriteLine(assgn2.GetSummary());
+        Console.WriteLine(assgn2.GetHomeworkList());
+
+        WritingAssignment assgn3 = new WritingAssignment("Agata", "War", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(assgn3.GetSummary());
+        Console.WriteLine(assgn3.GetWritingInformation());
     }
 }
