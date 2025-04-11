3cec98476d1f9d3c717956b6f3fff4dd0bd19d74
Barbara <5elementszone@gmail.com>
2025-04-11 16:30:05 +0200

    finished menu

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -24,6 +24,32 @@ class Program
                     BreathingActivity breathing = new BreathingActivity();
                     breathing.Run();
                     break;
+                
+                case "2":
+                    ReflectingActivity reflecting = new ReflectingActivity();
+                    reflecting.Run();
+                    break;
+                
+                case "3":
+                    ListingActivity listing = new ListingActivity();
+                    listing.Run();
+                    break;
+                
+                case "4":
+                    running = false;
+                    Console.WriteLine("Thank you for using the Mindfulness Program. Goodbye!");
+                    break;
+                
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a number from 1 to 4.");
+                    Thread.Sleep(2000);
+                    break;
+            }
+
+            if (running)
+            {
+                Console.WriteLine("\nPress Enter to return to the main menu...");
+                Console.ReadLine();
             }
         }
     }
