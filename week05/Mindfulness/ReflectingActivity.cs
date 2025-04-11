d53ae3a44568dbb29dfa2e228bcf87a40e4fc461
Barbara <5elementszone@gmail.com>
2025-04-11 18:07:21 +0200

    finiished reflecting class

--- week05/Mindfulness/ReflectingActivity.cs
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -1,35 +1,83 @@
-public class ReflectingActivity
+public class ReflectingActivity : Activity
 {
     private List<string> _prompts;
     private List<string> _questions;
 
-    public ReflectingActivity()
+    public ReflectingActivity() : base("Reflecting Activity", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.")
     {
+        _prompts = new List<string>
+        {
+            "Think of a time when you stood up for someone else.",
+            "Think of a time when you did something really difficult.",
+            "Think of a time when you helped someone in need.",
+            "Think of a time when you did something truly selfless."
+        };
 
+        _questions = new List<string>
+        {
+            "Why was this experience meaningful to you?",
+            "Have you ever done anything like this before?",
+            "How did you get started?",
+            "How did you feel when it was complete?",
+            "What made this time different than other times when you were not as successful?",
+            "What is your favorite thing about this experience?",
+            "What could you learn from this experience that applies to other situations?",
+            "What did you learn about yourself through this experience?",
+            "How can you keep this experience in mind in the future?"
+        };
     }
 
     public void Run()
     {
+        DisplayStartingMessage();
 
+        Console.Clear();
+        DisplayPrompt();
+
+        Console.WriteLine("\nNow consider the following quiestions:\n");
+        ShowSpinner(2);
+
+        DisplayQuestion();
+        
+        DisplayEndingMessage();
     }
 
     public string GetRandomPrompt()
     {
-        return "";
+        Random rand = new Random();
+        int index = rand.Next(_prompts.Count);
+        return _prompts[index];
     }
 
     public string GetRandomQuestion()
     {
-        return "";
+        Random rand = new Random();
+        int index = rand.Next(_questions.Count);
+        return _questions[index];
     }
 
     public void DisplayPrompt()
     {
-
+        string prompt = GetRandomPrompt();
+        Console.WriteLine("Consider the following prompt:");
+        Console.WriteLine($"---{prompt}---");
+        Console.WriteLine("\nWhen you have something in mind, press Enter to continue.");
+        Console.ReadLine();
+        Console.WriteLine("Get ready to reflect...");
+        ShowCountDown(3);
     }
 
     public void DisplayQuestion()
     {
-        
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            string question = GetRandomQuestion();
+            Console.Write($"> {question} ");
+            ShowSpinner(10);
+            Console.WriteLine("\n");
+        }
     }
 }
